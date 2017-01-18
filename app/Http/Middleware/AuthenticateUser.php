f02ed088c98f019a1ecb087295fc74e93c5cb9d1
theminer3746 <bwsminecraft@hotmail.com>
2017-01-18 21:15:01 +0700

    Create and register middleware to redirect unauthenticated user to login page

---
+++ app/Http/Middleware/AuthenticateUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Http\Controllers\User\LoginController;
+
+class AuthenticateUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if(LoginController::isLoggedIn()){
+            return $next($request);
+        }else{
+            redirect('/login');
+        }
+    }
+}
