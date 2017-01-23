a8538db1f16cefdb19896bfd18c299bb61365544
theminer3746 <bwsminecraft@hotmail.com>
2017-01-23 13:07:50 +0700

    Create method to render logout page Create method to log the user out

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -15,5 +15,15 @@ class LoginController extends Controller
     public static function isLoggedIn(){
         return (Session::get('user_logged_in') === true);
     }
+
+    public function showLogoutPage(){
+        return view('user.logout');
+    }
+
+    public function handleLogout(){
+        Session::flush();
+        Session::regenerate();
+
+        return redirect('/');
     }
 }
