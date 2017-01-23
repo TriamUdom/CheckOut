6dccd0670a4459eedfc90118de911367e18ca6b1
theminer3746 <bwsminecraft@hotmail.com>
2017-01-23 13:08:10 +0700

    Make login work

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\User;
 
+use Hash;
 use Session;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,6 +12,57 @@ class LoginController extends Controller
 {
     public function showLoginPage(Request $request){
         return view('user.login');
+    }
+
+    public function handleLoginRequest(Request $request){
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('username', $request->input('username'))->first();
+
+        $validateLoginRequest = $this->validateLoginRequest($request, $user);
+        if($validateLoginRequest !== true){
+            return $validateLoginRequest;
+        }else{
+            $this->reHashIfRequired($request, $user);
+        }
+
+        return $this->createLoginSession($request);
+    }
+
+    private function validateLoginRequest(Request $request, User $user){
+        if(count($user) !== 1){
+            return redirect('/login')->withErrors(['username' => 'Username not found']);
+        }
+
+        if(!Hash::check($request->input('password'), $user['password'])){
+            return redirect('/login')->withErrors(['password' => 'Incorrect password']);
+        }
+
+        // TODO : 2FA Authen goes here
+
+        return true;
+    }
+
+    private function reHashIfRequired(Request $request, User $user){
+        if(Hash::needsRehash($user['password'])){
+            User::where('username', $request->input('username'))->update([
+                'password' => Hash::make($request->input('password')),
+            ]);
+        }
+
+        return true;
+    }
+
+    private function createLoginSession(Request $request){
+        Session::put([
+            'username' => $request->input('username'),
+            'user_logged_in' => true,
+        ]);
+
+        return redirect('/');
     }
 
     public static function isLoggedIn(){
