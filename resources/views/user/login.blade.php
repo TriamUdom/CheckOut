86185c7eebf4835ccc7304de16f764ecd1318cb4
theminer3746 <bwsminecraft@hotmail.com>
2017-01-23 13:10:08 +0700

    Prototype login page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,1 +1,20 @@
-Hello
+@extends('templates.master')
+
+@section('content')
+    <form action="/login" method="POST">
+        <input type="text" name="username" placeholder="Username"><br />
+        <input type="password" name="password" placeholder="Password"><br />
+        {{ csrf_field() }}
+        <input type="submit" value="Login">
+    </form>
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+@stop
