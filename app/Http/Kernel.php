06c891677e148cb1a8fc1dc970f06a82080aac36
theminer3746 <bwsminecraft@hotmail.com>
2017-01-23 13:08:28 +0700

    Register unauth middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,6 +47,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth.user' => \App\Http\Middleware\AuthenticateUser::class,
+        'unauth.user' => \App\Http\Middleware\UnauthenticateUser::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
