3f1d715bd967ae679ee4bb96a6a4f7a6b8d20275
theminer3746 <bwsminecraft@hotmail.com>
2017-01-23 14:00:53 +0700

    Create method to test login

---
+++ tests/UserControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class UserControllerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function test_login()
+    {
+        $user = factory(App\User::class)->create();
+
+        $response = $this->call('POST', '/login', [
+            'username' => $user['username'],
+            'password' => 'secret',
+        ]);
+
+        $this->assertRedirectedTo('/');
+        $this->assertSessionHas('user_logged_in', true);
+    }
+}
