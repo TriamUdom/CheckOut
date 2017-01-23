6681d4d74a7c5972904038073e4cdd389c3a1c87
theminer3746 <bwsminecraft@hotmail.com>
2017-01-23 13:09:24 +0700

    Config mongodb

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
 
         /*
          * Application Service Providers...
