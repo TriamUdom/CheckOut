6607313939400f660e3fd3227cff03190303eb27
theminer3746 <bwsminecraft@hotmail.com>
2017-01-24 10:50:00 +0700

    Revert base url back

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = config('app.url');
+    protected $baseUrl = 'http://localhost';
 
     /**
      * Creates the application.
