b6bc76cd68282c26fab299ce888a1794bded54d6
theminer3746 <bwsminecraft@hotmail.com>
2017-01-31 14:13:50 +0700

    Create route for authorize student to leave

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,7 @@ Route::group(['middleware' => ['auth.user']], function(){
 
     Route::get('/logout', 'User\LoginController@showLogoutPage');
     Route::post('/logout', 'User\LoginController@handleLogout');
+
+    Route::get('/author', 'User\AuthorController@showAuthorPage');
+    Route::post('/author', 'User\AuthorController@handleAuthorization');
 });
